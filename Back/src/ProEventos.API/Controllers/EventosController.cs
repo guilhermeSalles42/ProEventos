b3974397f11c098ad94e787b9c776b8d65bbba8d
Guilherme Salles <guilherme.salles326@gmail.com>
2022-08-30 13:47:00 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Persistence;
+using ProEventos.Domain;
+using ProEventos.Application.Contratos
+
+
+
+
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        
+        private readonly IEventoService _eventoService;
+
+        public EventosController(IEventoService eventoService)
+        { 
+            _eventoService = eventoService;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento>Get()
+        {
+            try
+            {
+                    
+            }
+            catch (System.Exception)
+            {
+                    
+           throw;
+            }
+        }
+        
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.Id == id);       
+        }
+        
+        [HttpPost]
+        public string Post()
+        {
+           return "Exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+           return $"Exemplo de put com id = {id}";
+        }
+
+         [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+           return $"Exemplo de delete com id = {id}";
+        }
+    }
+}
